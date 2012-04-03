e998ffa834729274bd36c7c720c2007a8f4a8e3b
David Bahia <david.bahia@blueyonder.co.uk>
2012-04-03 01:45:38 +0100

    Test original full_title method using regular expressions

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,15 @@
+describe ApplicationHelper do
+
+  describe "full title" do
+    it "should include the page name" do
+      full_title("foo").should =~ /foo/
+    end
+
+    it "should include the base name" do
+      full_title("foo").should =~ /^Sample App/
+    end
+    it "should not include a bar for the home page" do
+      full_title("").should_not =~ /\|/
+    end
+  end
+end
