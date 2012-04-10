ef6f98a38810a808de02c9af6ec5f3438ee26afd
David Bahia <david.bahia@blueyonder.co.uk>
2012-04-10 14:28:23 +0100

    Add view test for successfully saving a user

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -48,6 +48,15 @@ describe "User pages" do
           click_button "Create my account"
         end.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button "Create my account" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Successfully created user') }
+      end
+
     end
 
   end   # Signup
