dcab0b0d86085aa16cfc01dcce514512f3f301e5
David Bahia <david.bahia@blueyonder.co.uk>
2012-05-07 19:34:05 +0100

    naive user status feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
@@ -48,6 +48,7 @@ class User < ActiveRecord::Base
   def unfollow!(other_user)
     relationships.find_by_followed_id(other_user.id).destroy
   end
+
   private
 
     def create_remember_token
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -168,10 +168,21 @@ describe User do
       let(:unfollowed_micropost) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_micropost) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
 
     describe "following" do
